c83b4002effd58a92925243e15f87a63d07b6522
Davi <davicamiloribeiro@gmail.com>
2022-02-21 16:40:38 -0300

    Rota para página não encontrada

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import main from './routes/index';
 import painel from './routes/painel';
 
@@ -6,5 +6,9 @@ const server = express();
 
 server.use(main);
 server.use('/painel', painel);
+
+server.use((req: Request, res: Response) => {
+    res.status(404).send('Página não encontrada!');
+});
 
 server.listen(3000);
