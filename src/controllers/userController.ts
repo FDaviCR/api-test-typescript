7cfb1bf715823620af89a6e3dd30ba8b53ea5c9d
Davi <davicamiloribeiro@gmail.com>
2022-03-04 17:09:44 -0300

    Adicionando comentarios

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,11 @@ import { User } from '../models/User';
 import Users from '../models/Users';
 
 export const allUsers = async (req: Request, res: Response) => {
+    
+    // Banco MySQL
     let users = await User.findAll();
+
+    // Banco MongoDB
     let usuarios = await Users.find({});
 
     console.log("Users: ", usuarios);
