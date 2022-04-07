ad6d831056d6d2d4d3814a79d19f9dd582f650ae
Davi <davicamiloribeiro@gmail.com>
2022-04-07 19:53:13 -0300

    Configuração básica do multer

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import { Router, Request, Response } from 'express';
+import multer from 'multer';
 const router = Router();
 
 import * as UserController from '../controllers/userController';
+
+const upload = multer({
+    dest: './temp'
+});
 
 router.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
