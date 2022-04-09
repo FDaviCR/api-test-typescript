b3e98489b0373d93e7f44ff2538da03f608a2608
Davi <davicamiloribeiro@gmail.com>
2022-04-09 17:16:10 -0300

    Definindo tipo para as imagens

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express';
 
 export const uploadFile = async (req: Request, res: Response) => {
-    const files = req.files as {[fieldname: string]: Express.Multer.File[]};
+    type UploadTypes = {
+        avatar: Express.Multer.File[],
+        gallery: Express.Multer.File[]
+    }
+
+    const files = req.files as UploadTypes;
+    
     console.log("Avatar", files.avatar );
     console.log("Gallery", files.gallery );
     
