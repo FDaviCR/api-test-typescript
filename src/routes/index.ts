3fc21ddf96c6368a129698da13d5766fce057db8
Davi <davicamiloribeiro@gmail.com>
2022-04-10 17:44:26 -0300

    Validação de arquivos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,28 @@ const router = Router();
 import * as UserController from '../controllers/userController';
 import * as ApiController from '../controllers/apiController';
 
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './temp');
+    },
+    filename: (req, file, cb) => {
+        let randomName = Math.floor(Math.random() * 99999);
+        cb(null, `${randomName+Date.now()}+.jpg`);
+    }
+});
+
 const upload = multer({
-    dest: './temp'
+    storage: storageConfig,
+    fileFilter: (req, file, cb) => {
+        const allowed: string[] = ['image/jpg', 'image/jpeg', 'image/png'];
+
+        if(allowed.includes(file.mimetype)){
+            cb(null, true);
+        }else{
+            cb(null, false);
+        }
+    },
+    limits: { fieldSize: 2000000 }
 });
 
 router.get('/', (req: Request, res: Response) => {
@@ -14,6 +34,7 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 router.get('/users', UserController.allUsers)
+
 router.post('/upload', upload.fields([
     { name: 'avatar', maxCount: 1},
     { name: 'gallery', maxCount: 3}
